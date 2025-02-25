303723111eeaf765b41e2dd3840385cd179fff4c
Gustavo Santana <gusgsus10.gssn@gmail.com>
2025-02-25 14:00:55 -0600

    Agrego mi archivo que imprime un saludo

--- practicas/3/SantanaGustavo/saludo.c
+++ practicas/3/SantanaGustavo/saludo.c
@@ -2,9 +2,14 @@
 
 
 int main (void) {
-
+  char nombre[32];
+  printf("Cuál es tu nombre? ");
+  scanf("%s", &nombre);
   printf("Hola a todos papus.com\n");
+  printf("Hola a %s, a ti también.\n", nombre);
 
   return(0);
 }
+
+
 
